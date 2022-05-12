6725930a562e7dd9cc2e1bb8c8e58e008060575f
Aman <ak669212@gmail.com>
2022-05-12 07:42:07 +0530

    portfolio done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 const swup = new Swup();
+
+
+const ctaButton = document.querySelector('.cta .get-con');
+// const dataValue = ctaButton.getAttribute('data-value');
+// if(dataValue === 'Projects'){
+//     ctaButton.textContent = 'Projects';
+//     console.log(ctaButton);
+// }
+// else if(dataValue !== 'Projects'){
+//     ctaButton.textContent = 'Get in Touch';
+// }
+
+    
+swup.on('contentReplaced', function() {
+    if (window.location.href.indexOf("contact") > -1) {
+        ctaButton.textContent = "Projects";
+    } else{
+        ctaButton.textContent = "Get in Touch";
+    }
+});
